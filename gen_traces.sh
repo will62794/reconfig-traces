4416b025063800f808a1b1b408c463dcadc47f66
William Schultz <william.schultz@mongodb.com>
2025-06-05 13:45:44 -0400

    Update traces

--- gen_traces.sh
+++ gen_traces.sh
@@ -7,6 +7,7 @@ traces="t1 t3-oplog-commitment t4"
 for t in $traces; do
     $tlc -deadlock -noGenerateSpecTE -config MongoRaftReconfig_$t.cfg MongoRaftReconfig.tla | tee traces/$t.txt
     grep -A 10000 "Invariant.*is violated" traces/$t.txt | grep -v "configTerm" |\
-     grep -B 10000 "states generated" | grep -v "states generated" > traces/$t-cleaned.txt
+     grep -B 10000 "states generated" | grep -v "states generated" |\
+      sed "s/line.*of module/of/g" > traces/$t-cleaned.txt
     echo "Generated traces/$t-cleaned.txt"
 done
